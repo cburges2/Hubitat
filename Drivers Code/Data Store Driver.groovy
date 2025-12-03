a313a8fb079879c33e4116e377742031764b9d60
Chris Burgess <66129215+cburges2@users.noreply.github.com>
2025-12-03 10:14:27 -0500

    Add files via upload

--- Drivers Code/Data Store Driver.groovy
+++ Drivers Code/Data Store Driver.groovy
@@ -1,7 +1,7 @@
 /*
 	Data Store Driver
 
-	Copyright 2025 -> C. Burgess
+	Copyright 2025 -> C. Burgess   https://raw.githubusercontent.com/cburges2/Hubitat/refs/heads/main/Drivers%20Code/Data%20Store%20Driver.groovy
 */
 
 metadata {
@@ -26,22 +26,18 @@ metadata {
 
 	preferences {
 		input( name: "logEnable", type:"bool", title: "Enable debug logging",defaultValue: false)
-		input( name: "txtEnable", type:"bool", title: "Enable descriptionText logging", defaultValue: true)
-        input name: "autoOff", type: "enum", description: "Automatically sets motion inactive after selected time.", title: "Enable Auto-inactive", options: [[0:"Disabled"],[1:"1s"],[2:"2s"],[5:"5s"],[10:"10s"],[20:"20s"],[30:"30s"],[60:"1m"],[120:"2m"],[300:"5m"],[600:"10m"],[1800:"30m"],[3200:"60m"]], defaultValue: 0
 	}
 }
 
 
 def installed() {
 	log.warn "installed..." 
-    device.updateSetting("txtEnable",[type:"bool",value:true])
 	initialize()
 }
 
 def updated() {
 	log.info "updated..."
 	log.warn "debug logging is: ${logEnable == true}"
-	log.warn "description logging is: ${txtEnable == true}"
 	if (logEnable) runIn(1800,logsOff)
 	initialize()
 }
@@ -65,10 +61,4 @@ def setAttribute(name, value) {
 
 private logDebug(msg) {
 	if (settings?.logEnable) log.debug "${msg}"
-}
-
-private getDescriptionText(msg) {
-	def descriptionText = "${device.displayName} ${msg}"
-	if (settings?.txtEnable) log.info "${descriptionText}"
-	return descriptionText
 }
